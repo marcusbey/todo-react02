519e36cc8b7a1c878b19aa01b725bf25332ba9fd
Romain BOBOE <rboboe@gmail.com>
2020-09-10 04:26:40 -0400

    creating project with react

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <div className="container">
+
+          <div id="task-container">
+              <div  id="form-wrapper">
+                  <form  id="form">
+                    <div className="flex-wrapper">
+                        <div style={{flex: 6}}>
+                            <input onChange='' className="form-control" id="title" value='' type="text" name="title" placeholder="Add task.." />
+                         </div>
+
+                         <div style={{flex: 1}}>
+                            <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                        </div>
+                    </div>
+                  </form>
+
+                </div>
+
+              </div>
+
+          </div>
+
   );
 }
 
