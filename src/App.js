6856ab1378433d917621688360941c4979433b59
Romain BOBOE <rboboe@gmail.com>
2020-09-16 12:04:58 -0400

    fixing the url to fetch task

--- src/App.js
+++ src/App.js
@@ -1,60 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-constructor(props){
-  super(props);
-    this.state = {
-      todoList:[],
-      activeItem:{
-        id: null, 
-        title: '',
-        completed:false,
-        },
-      editing: false,
-    }
-    this.fetchTasks = this.fetchTasks.bind(this)
-};
-// lifecycle methode
-componentWillMount(){
-  this.fetchTasks()
-}
+  constructor(props){
+    super(props);
+      this.state = {
+        todoList:[],
+        activeItem:{
+          id: null, 
+          title: '',
+          completed:false,
+          },
+        editing: false,
+      }
+      this.fetchTasks = this.fetchTasks.bind(this);
+  }; 
+  // lifecycle methode
+  componentWillMount(){
+    this.fetchTasks();
+  }
+ 
+  fetchTasks(){
+    console.log('Fetchning...');
+    fetch('http://127.0.0.1:8000/api/task-list/')
+    .then(response => response.json())
+    .then(data => 
+      console.log('Data:', data)
+      ) 
+  }
 
-fetchTasks(){
-  console.log('Fetchning...')
-}
 
-fetchTasks(){
+    render() {
+      return (
+          <div className="container">
 
-}
-
-  render() {
-    return (
-        <div className="container">
-
-          <div id="task-container">
-              <div  id="form-wrapper">
-                  <form  id="form">
-                    <div className="flex-wrapper">
-                        <div style={{flex: 6}}>
-                            <input onChange='' className="form-control" id="title" value='' type="text" name="title" placeholder="Add task.." />
-                         </div>
-
-                         <div style={{flex: 1}}>
-                            <input id="submit" className="btn btn-warning" type="submit" name="Add" />
-                        </div>
-                    </div>
-                  </form>
-
+            <div id="task-container">
+                <div  id="form-wrapper">
+                    <form  id="form">
+                      <div className="flex-wrapper">
+                          <div style={{flex: 6}}>
+                              <input className="form-control" id="title"  type="text" name="title" placeholder="Add task.." />
+                              {/*onChange="" */}
+                          </div>
+                          <div style={{flex: 1}}>
+                              <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                          </div>
+                      </div>
+                    </form>
+                  </div>
                 </div>
-
-              </div>
-
-          </div>
-
-  );
-}
+         </div>
+    );
+  }
 }
 export default App;
- 
